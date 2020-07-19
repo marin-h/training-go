6ef20808e158fbbf96fbfccd250d7679cc6eca45
marin-h <marinahuberman@gmail.com>
2020-07-19 16:18:50 -0300

    add kindersquare exercise

---
+++ kindersquare.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	arr := []int{3, 1, 4}
+	kinderSquare(arr)
+}
+
+func kinderSquare(arr []int) {
+	for _, value := range arr {
+		if condition(value) {
+			fmt.Println(calculate(value))
+		}
+	}
+}
+
+func calculate(value int) int {
+	return int(math.Pow(float64(value), 2) + 10)
+}
+
+func condition(value int) bool {
+	remain := calculate(value) % 10
+	return remain != 5 && remain != 6
+}
