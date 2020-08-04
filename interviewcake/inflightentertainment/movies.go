d59241675fe489a00f492fff9196ab241f8c9907
marin-h <marinahuberman@gmail.com>
2020-08-04 19:03:24 -0300

    add find two elements matching condition using hashmap

---
+++ interviewcake/inflightentertainment/movies.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	flightLength := 311
+	movieLengths := []int{50, 300, 121, 190, 90}
+	fmt.Println(getMovies(flightLength, movieLengths))
+}
+
+func getMovies(flightLength int, movieLengths []int) bool {
+
+	// find if two movies together sum exactly to flightlength in O(n) runtime
+	movieMap := map[int]int{}
+	for i, length := range movieLengths {
+		if length < flightLength {
+			movieMap[length] = i
+		}
+	}
+	for length, _ := range movieMap {
+		if _, matches := movieMap[flightLength-length]; matches {
+			fmt.Println(movieMap[flightLength-length], movieMap[length])
+			return true
+		}
+	}
+	return false
+}
