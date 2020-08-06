79791ff0c494529ef8080a571ccb5b77eb2d6f39
marin-h <marinahuberman@gmail.com>
2020-08-06 19:37:35 -0300

    add breadth first search

---
+++ grokking-algorithms/graphs/bfs.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	graph := map[string][]string{}
+	graph["you"] = []string{"alice", "bob", "claire"}
+	graph["bob"] = []string{"anuj", "peggy"}
+	graph["alice"] = []string{"peggy"}
+	graph["claire"] = []string{"thom", "jonny"}
+	graph["anuj"] = []string{}
+	graph["peggy"] = []string{}
+	graph["thom"] = []string{}
+	graph["jonny"] = []string{}
+
+	fmt.Println(bfs(graph))
+}
+
+func bfs(graph map[string][]string) string {
+
+	visited := map[string]bool{}
+	queue := append([]string{}, graph["you"]...)
+
+	for len(queue) > 0 {
+
+		person := queue[0]
+		queue = queue[1:]
+
+		if visited[person] {
+			fmt.Println(person, "already visited")
+			continue
+		}
+		if matches(person) {
+			return "found " + person
+		} else {
+			visited[person] = true
+			queue = append(queue, graph[person]...)
+		}
+	}
+	return "not found"
+}
+
+func matches(name string) bool {
+	return name[0:1] == "t"
+}
